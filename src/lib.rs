ce3bdde2d55b8a087f7281499bff8cb9530a9e2a
Rene Eichhorn <rene.eichhorn@mni.thm.de>
2016-10-20 20:22:27 +0200

    removed scaling

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@ extern crate byteorder;
 
 pub mod neural_network;
 pub mod evolution;
-pub mod scaling;
 
 pub use neural_network::NeuralNetwork;
 pub use neural_network::NeuronType;
@@ -17,5 +16,3 @@ pub use neural_network::Instance;
 pub use neural_network::cpu::CpuInstance;
 
 pub use evolution::*;
-
-pub use scaling::*;
